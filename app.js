acafb58db96bbeaf32aa53bea0c2d2fa5128d713
aniruddha <ani@dctacademy.com>
2020-01-30 12:56:52 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,90 @@
+const express = require('express')
+const fs = require('fs')
+const app = express() 
+const port = 3033
+app.use(express.json())
+
+app.get('/tasks', (req, res) => {
+    fs.readFile('./data.json', 'utf-8', (err, data) => {
+        if(err) {
+            res.json(err) 
+        } else {
+            const tasks = JSON.parse(data) 
+            res.json(tasks)
+        }
+    })
+})
+
+app.post('/tasks', (req, res) => {
+    const body = req.body 
+    fs.readFile('./data.json', 'utf-8', (err, data) => {
+        if(err) {
+            res.json(err) 
+        } else {
+            const tasks = JSON.parse(data) 
+            tasks.push(body) 
+            fs.writeFile('./data.json', JSON.stringify(tasks), () => {
+                res.json(body)
+            })
+        }
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const id = req.params.id 
+    fs.readFile('./data.json', 'utf-8', (err, data) => {
+        if(err) {
+            res.json(err) 
+        } else {
+            const tasks = JSON.parse(data) 
+            const task = tasks.find(task => task.id == id) 
+            if(task) {
+                res.json(task) 
+            } else { 
+                res.json({})
+            }
+        }
+    })
+})
+
+app.put('/tasks/:id', (req, res) => {
+    const id = req.params.id 
+    const body = req.body 
+    fs.readFile('./data.json', 'utf-8', (err, data) => {
+        if(err) {
+            res.json(err) 
+        } else {
+            const tasks = JSON.parse(data) 
+            const task = tasks.find(task => task.id == id)
+            if(task) {
+                Object.assign(task, body)
+                fs.writeFile('./data.json', JSON.stringify(tasks), () => {
+                    res.json(task) 
+                })
+            } else {
+                res.json({})
+            }
+        }
+    })
+})
+
+app.delete('/tasks/:id', (req, res) => {
+    const id = req.params.id 
+    fs.readFile('./data.json', 'utf-8', (err, data) => {
+        if(err) {
+            res.json(err) 
+        } else {
+            let tasks = JSON.parse(data) 
+            const task = tasks.find(task => task.id == id) 
+            tasks = tasks.filter(task => task.id != id) 
+            fs.writeFile('./data.json', JSON.stringify(tasks), () => {
+                res.json(task)
+            })
+        }
+    })
+})
+
+
+app.listen(port, () => {
+    console.log('listening on port', port)
+})
