1df7842c1ccbeeed1e469cda4c07c37518840bb6
aniruddha <ani@dctacademy.com>
2020-01-30 12:59:37 +0530

    cors enabled

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const fs = require('fs')
+const cors = require('cors')
 const app = express() 
 const port = 3033
+
+app.use(cors())
 app.use(express.json())
 
 app.get('/tasks', (req, res) => {
